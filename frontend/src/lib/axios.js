5645c17c1a030ed4a09197ad0e2b630f1dbb156e
roaa12114 <roaa12114@gmail.com>
2025-10-07 23:40:07 +0200

    Connect frontend with Railway backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,11 @@ import axios from "axios";
 
 
 // in production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://note-app-backend-production-ff22.up.railway.app/api";
+
 
 const api = axios.create({
   baseURL: BASE_URL,
